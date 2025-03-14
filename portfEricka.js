d22594d54253275ba142b52d3480cc7f12fce0fb
Dario <darioflo19@icloud.com>
2025-03-13 18:34:32 -0600

    Terminado!!

--- portfEricka.js
+++ portfEricka.js
@@ -1,24 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.animada');
-    const paragraphs = document.querySelectorAll('.animadad');
+    const elements = document.querySelectorAll('.animada, .animadad');
 
     const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
+        entries.forEach((entry, index) => {
             if (entry.isIntersecting) {
-                if (entry.target.tagName.toLowerCase() === 'img') {
+                if (entry.target.classList.contains('animada')) {
+                    entry.target.style.animationDelay = `${index * 0.2}s`;
                     entry.target.classList.add('animate__animated', 'animate__fadeInRight');
-                } else if (entry.target.tagName.toLowerCase() === 'p') {
+                } else if (entry.target.classList.contains('animadad')) {
+                    entry.target.style.animationDelay = `${index * 0.2}s`;
                     entry.target.classList.add('animate__animated', 'animate__fadeInLeftBig');
                 }
                 observer.unobserve(entry.target);
             }
         });
     }, {
-        threshold: 0.2
+        threshold: 0.1
     });
 
-    images.forEach(image => {
-        observer.observe(image);
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const icons = document.querySelectorAll('.icon-service');
+    const paragraphs = document.querySelectorAll('.text-service');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains('icon-service')) {
+                    entry.target.classList.add('animate__animated', 'animate__zoomInLeft');
+                } else if (entry.target.classList.contains('text-service')) {
+                    entry.target.classList.add('animate__animated', 'animate__zoomInRight');
+                }
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    icons.forEach(icon => {
+        observer.observe(icon);
     });
 
     paragraphs.forEach(paragraph => {
